d4c5719203cc2a70ba6522147f41f2d181f87aab
Ilya Shevchenko <id.shev@yandex.ru>
2022-11-09 13:53:50 +0400

    feat: add authentications
    Add working authentications options for ydb driver.

--- src/driver/ydb/YdbConnectionOptions.ts
+++ src/driver/ydb/YdbConnectionOptions.ts
@@ -1,4 +1,10 @@
 import { BaseDataSourceOptions } from "../../data-source/BaseDataSourceOptions"
+import {
+    AnonymousAuthOptions,
+    TokenAuthOptions,
+    IamAuthOptions,
+    MetadataAuthOptions,
+} from "./AuthenticationOptions"
 
 /**
  * YDB specific connection options.
@@ -8,13 +14,44 @@ export interface YdbConnectionOptions extends BaseDataSourceOptions {
      * Database type.
      */
     readonly type: "ydb"
-    
-    // TODO: add all options and checkmark if it used or not
+
+    /**
+     * Endpoint name to connect to.
+     */
+    readonly endpoint: string
 
     /**
      * Database name to connect to.
-     * 
-     * **NOT USED YET**, just to avoid failing the tests
      */
-     readonly database?: string
+    readonly database: string
+
+    /**
+     * Authentication options
+     *
+     * Read more [here](https://ydb.tech/en/docs/concepts/auth)
+     */
+    readonly authentication:
+        | AnonymousAuthOptions
+        | TokenAuthOptions
+        | IamAuthOptions
+        | MetadataAuthOptions
+
+    /**
+     * Timeout to create connection in milliseconds
+     */
+    readonly connectTimeout: number
+
+    /**
+     * gRPC client options. [Read more](https://grpc.github.io/grpc/core/group__grpc__arg__keys.html)
+     */
+    readonly gRpcClientOptions?: { [key: string]: any }
+
+    /**
+     * Session pool settings
+     */
+    readonly poolSettings?: {
+        minLimit?: number
+        maxLimit?: number
+        keepAlivePeriod?: number
+    }
 }
